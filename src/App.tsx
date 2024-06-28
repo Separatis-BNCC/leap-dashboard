56e154da4987416f088b52de23ae64d2536a8591
Specticall <josephyusmita@gmail.com>
2024-06-28 11:52:18 +0700

    [BNCC-22]: Initialized shadcn & tailwind

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Button } from "./components/ui/Button";
+export default function App() {
+  return (
+    <div className="grid place-items-center min-h-screen">
+      <Button variant={"accent"}>Click Me</Button>
+    </div>
+  );
+}
